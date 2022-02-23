29b6d6f562595ed258ffe252f9dd8af8bf306857
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-02-23 17:01:53 -0300

    teste

--- main.js
+++ main.js
@@ -1,48 +1,45 @@
-// setup canvas
-class Ball {
-  constructor ( x, y, velX, velY, color, size ) {
-      this.x = x
-      this.y = y
-      this.velX = velX
-      this.vely = velY
-      this.color = color
-      this.size = size
-  }
-
-  draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
-      ctx.fill()
-  }
-  update() {
-      if ((this.x + this.size) >= width) {
-        this.velX = -(this.velX);
-      }
-    
-      if ((this.x - this.size) <= 0) {
-        this.velX = -(this.velX);
-      }
-    
-      if ((this.y + this.size) >= height) {
-        this.velY = -(this.velY);
-      }
-    
-      if ((this.y - this.size) <= 0) {
-        this.velY = -(this.velY);
-      }
-    
-      this.x += this.velX;
-      this.y += this.velY;
-  }
-}
-
-
 const canvas = document.querySelector('canvas');
-window.ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+// setup canvas
+function Ball( x, y, velX, velY, color, size ) {
+  this.x = x
+  this.y = y
+  this.velX = velX
+  this.vely = velY
+  this.color = color
+  this.size = size
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+    ctx.fill()
+}
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
 
 // function to generate random number
 
@@ -61,32 +58,34 @@ let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
 
 
 let balls = [];
-while (balls.length < 25) {
-  let size = random(10,20);
-  let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-7,7),
-    random(-7,7),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-    size
-  );
-
-  balls.push(ball);
-}
-
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
+  while (balls.length < 25) {
+    let size = random(10,20);
+    let ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      size
+    );
+
+  balls.push(ball);
+}
+
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+    console.log(balls.length)
+      
   }
-
-  // window.requestAnimationFrame(loop);
+  console.log("oi")
+  window.requestAnimationFrame(loop);
 }
 
 loop()
