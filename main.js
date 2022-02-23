a8a0d660d4047470bbb46fc5ef9a0d1cdce9533e
Rodrigo Méro <meroo.rodrigo@gmail.com>
2022-02-23 15:48:54 -0300

    added the basic functions

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
 // setup canvas
+class Ball {
+  constructor ( x, y, velX, velY, color, size ) {
+      this.x = x
+      this.y = y
+      this.velX = velX
+      this.vely = velY
+      this.color = color
+      this.size = size
+  }
+
+  draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+      ctx.fill()
+  }
+  update() {
+      if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+      }
+    
+      if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+      }
+    
+      this.x += this.velX;
+      this.y += this.velY;
+  }
+}
+
 
 const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+window.ctx = canvas.getContext("2d");
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
@@ -18,3 +56,37 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+
+
+let balls = [];
+while (balls.length < 25) {
+  let size = random(10,20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  // window.requestAnimationFrame(loop);
+}
+
+loop()
